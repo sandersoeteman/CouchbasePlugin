7a057a1818fd7a2f92889eb7f65295207b10ef38
Sander Soeteman <sander@auger.nl>
2014-10-29 15:22:55 +0100

    nog een clobber poging

--- www/PlanbordCouchPlugin.js
+++ www/PlanbordCouchPlugin.js
@@ -1,22 +1,23 @@
-window.couch = function(methodName, paramArray) {
-    return new Promise(function(resolve, reject) {
+module.exports = {
+    couch: function(methodName, paramArray) {
+        return new Promise(function(resolve, reject) {
 
-        if (typeof cordova === 'undefined' || !cordova.exec) {
-            reject(Error('Cordova undefined')); 
-        }
-        else {
-            cordova.exec(
-                function (result) {
-                    resolve(result);
-                },
-                function (err) { 
-                    reject(Error(err));
-                },
-                'PlanbordCouch',
-                methodName,
-                paramArray
-            );
-        }
-    });
-
-};
+            if (typeof cordova === 'undefined' || !cordova.exec) {
+                reject(Error('Cordova undefined')); 
+            }
+            else {
+                cordova.exec (
+                    function (result) {
+                        resolve(result);
+                    },
+                    function (err) { 
+                        reject(Error(err));
+                    },
+                    'PlanbordCouch',
+                    methodName,
+                    paramArray
+                );
+            }
+        });
+    }
+}
