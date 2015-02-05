53e652a33e34da7a568e19a0f817997daa784088
Sander Soeteman <sander@auger.nl>
2015-02-05 12:43:08 +0100

    begin implementatie plugin

---
+++ src/android/ImagesFilter.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.Map;
+
+import com.couchbase.lite.ReplicationFilter;
+import com.couchbase.lite.SavedRevision;
+
+
+public class ImagesFilter implements ReplicationFilter {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean filter(SavedRevision revision, Map<String, Object> params) {
+		Integer gebruikerID = (Integer) params.get("GebruikerID");
+		String type = (String) params.get("type");
+		
+		if(gebruikerID == -1) {
+			return false;
+		}
+		
+		if(!(type.equals("image") || type.equals("user_image"))) {
+			return false;
+		}
+		
+		List<String> gebruikers = (List<String>) params.get("Gebruikers");
+		
+		if(gebruikers == null || gebruikers.size() == 0) {
+			return false;
+		}
+		
+		if(gebruikers.contains(gebruikerID)) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}
